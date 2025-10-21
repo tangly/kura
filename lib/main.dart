774c16919c184250a6a96c1ff00cb8d6fe0a8c43
stephane <stephane.info@gmail.com>
2025-10-21 11:23:08 +0200

    Update application name

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Medication Manager',
+      title: 'KURA',
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: ThemeMode.system,
